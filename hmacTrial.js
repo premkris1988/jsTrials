33e1352248d9922532dca27aed63403e8b96f2ea
prem.kris1988 <prem.kris1988@gmail.com>
2020-07-18 20:56:20 +0530

    getMethod using HMAC

--- hmacTrial.js
+++ hmacTrial.js
@@ -47,6 +47,34 @@ var hmacModule = (function hmacModule(CryptoJS) {
                         }else{ alert(http.responseText);}
                     }
                 http.send(params);
-            }
+            },
+            getTenureDetails:function(tenure_identifier){
+                var APPID = 'df9d7335-a724-4dce-94a2-1201c9a9c4ac';
+                var APPKey ='GWvxfbMbM3MpMoGkVSDxW3hnkSMmU/3y2KqhcvYK0NI='; 
+                var data='';
+                var requestHttpMethod='GET';
+                var nonce = uuidv4().replace(/-/g,'');
+                var ms = new Date().getTime();
+                var timeStamp = Math.floor(ms/1000);
+                var requestContentHash = CryptoJS.MD5(data);
+                var requestContentBase64String = CryptoJS.enc.Base64.stringify(requestContentHash);            
+                var url = encodeURIComponent('https://api.bananiapp.com/api/TenantSite/GetTenureDetails?tenure_identifier=eIhY5fM2');
+                var signatureRawData = APPID+requestHttpMethod+url.toLowerCase()+timeStamp+nonce+requestContentBase64String;
+                var secret_key = CryptoJS.enc.Base64.parse(APPKey);
+                var hmac = CryptoJS.HmacSHA256(signatureRawData, secret_key);
+                var requestSignatureBase64String = CryptoJS.enc.Base64.stringify(hmac); 
+                var hmac_header = APPID+':'+requestSignatureBase64String+':'+nonce+':'+timeStamp;
+                var http = new XMLHttpRequest();
+                var get_url = 'https://api.bananiapp.com/api/TenantSite/GetTenureDetails?tenure_identifier=eIhY5fM2';
+                http.open('GET', get_url, true);
+                http.setRequestHeader('Content-type', 'application/json');
+                http.setRequestHeader('hmacauth',hmac_header);
+                http.onreadystatechange = function() {//Call a function when the state changes.
+                    if(http.readyState == 4 && http.status == 200) {
+                            console.log(http.responseText);
+                            }else{ console.log(http.responseText);}
+                        }
+                    http.send();
+                }
     };    
 }(CryptoJS));
